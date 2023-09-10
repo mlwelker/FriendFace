19471edd7f69790cb89ed97d8da7ad1c32283a68
mlwelker <nutria.watts.0g@icloud.com>
2023-09-09 23:51:10 -0500

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Michael Welker on 2023-09-09.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
